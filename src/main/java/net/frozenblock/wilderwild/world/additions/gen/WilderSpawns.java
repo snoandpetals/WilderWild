018f3e080db872cb23acb18eec1f7008dad2ca01
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-30 15:43:51 -0500

    MobCategory

--- src/main/java/net/frozenblock/wilderwild/world/additions/gen/WilderSpawns.java
+++ src/main/java/net/frozenblock/wilderwild/world/additions/gen/WilderSpawns.java
@@ -2,7 +2,8 @@ package net.frozenblock.wilderwild.world.additions.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.frozenblock.wilderwild.misc.WilderEnumValues;
+import net.frozenblock.lib.mobcategory.api.FrozenMobCategories;
+import net.frozenblock.wilderwild.misc.WilderSharedConstants;
 import net.frozenblock.wilderwild.registry.RegisterEntities;
 import net.frozenblock.wilderwild.tag.WilderBiomeTags;
 import net.minecraft.world.entity.EntityType;
@@ -11,25 +12,25 @@ import net.minecraft.world.level.biome.Biomes;
 
 public final class WilderSpawns {
 
-	public static void addFireflies() {
-		BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE_DURING_DAY),
-				WilderEnumValues.FIREFLIES, RegisterEntities.FIREFLY, 12, 2, 4);
+    public static void addFireflies() {
+        BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE_DURING_DAY),
+				FrozenMobCategories.getCategory(WilderSharedConstants.MOD_ID, "fireflies"), RegisterEntities.FIREFLY, 12, 2, 4);
 
-		BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE_CAVE),
-				WilderEnumValues.FIREFLIES, RegisterEntities.FIREFLY, 5, 2, 4);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE_CAVE),
+				FrozenMobCategories.getCategory(WilderSharedConstants.MOD_ID, "fireflies"), RegisterEntities.FIREFLY, 5, 2, 4);
 
-		BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE),
-				WilderEnumValues.FIREFLIES, RegisterEntities.FIREFLY, 5, 1, 2);
-	}
+        BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.FIREFLY_SPAWNABLE),
+				FrozenMobCategories.getCategory(WilderSharedConstants.MOD_ID, "fireflies"), RegisterEntities.FIREFLY, 5, 1, 2);
+    }
 
-	public static void addJellyfish() {
-		BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.HAS_JELLYFISH),
-				WilderEnumValues.JELLYFISH, RegisterEntities.JELLYFISH, 2, 1, 1);
-	}
+    public static void addJellyfish() {
+        BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.HAS_JELLYFISH),
+				FrozenMobCategories.getCategory(WilderSharedConstants.MOD_ID, "jellyfish"), RegisterEntities.JELLYFISH, 2, 1, 1);
+    }
 
 	public static void addTumbleweed() {
 		BiomeModifications.addSpawn(BiomeSelectors.tag(WilderBiomeTags.HAS_TUMBLEWEED_ENTITY),
-				WilderEnumValues.TUMBLEWEED, RegisterEntities.TUMBLEWEED, 30, 1, 1);
+				FrozenMobCategories.getCategory(WilderSharedConstants.MOD_ID, "tumbleweed"), RegisterEntities.TUMBLEWEED, 30, 1, 1);
 	}
 
 	public static void addRabbits() {
