473c0fe2f9770e148866c4a5742b3fdac58313f4
Treetrain1 <treetrain1@gmail.com>
2024-03-12 20:52:23 -0500

    fix

--- src/main/java/net/frozenblock/wilderwild/datafix/wilderwild/WWDataFixer.java
+++ src/main/java/net/frozenblock/wilderwild/datafix/wilderwild/WWDataFixer.java
@@ -56,7 +56,7 @@ public class WWDataFixer implements DataFixerEntrypoint {
 			DSL::remainder
 		);
 		registry.register(
-			WilderSharedConstants.id("metal_chest"),
+			WilderSharedConstants.id("stone_chest"),
 			() -> DSL.optionalFields("Items", DSL.list(References.ITEM_STACK.in(schema)))
 		);
 		registry.register(
